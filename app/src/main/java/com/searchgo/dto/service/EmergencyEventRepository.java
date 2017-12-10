6bed08d315f58bfb445d2b885dc7ca9f98ced788
tamartwe <tamar.twena@gmail.com>
2017-12-10 11:03:33 +0200

    add gson

---
+++ app/src/main/java/com/searchgo/dto/service/EmergencyEventRepository.java
@@ -0,0 +1,8 @@
+package com.searchgo.dto.service;
+
+/**
+ * Created by tamar.twena on 12/4/2017.
+ */
+
+public class EmergencyEventRepository {
+}
---
+++ app/src/main/java/com/searchgo/nativeServices/BaseService.java
@@ -0,0 +1,8 @@
+package com.searchgo.nativeServices;
+
+/**
+ * Created by tamar.twena on 12/4/2017.
+ */
+
+public class BaseService {
+}
